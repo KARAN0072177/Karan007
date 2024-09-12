2a67e1093faed84bb004d9c1b5e15731cec5a0e0
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:18:24 +0530

    Add files via upload
    basic array syntax in java script

---
+++ array.js
@@ -0,0 +1,6 @@
+let languages = ["C++", "React", "Express.js"];
+console.log(languages[0]); // Access first element  (index starts with 0)
+
+// Adding a new element
+languages.push("Node.js");
+console.log(languages);
