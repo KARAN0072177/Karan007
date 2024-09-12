79bce19c54afaa67570258e1e6fd6a217156a750
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:09:20 +0530

    Create operations.js
    basic operations in java script

---
+++ operations.js
@@ -0,0 +1,9 @@
+// basic operations
+
+let a = 10;
+let b = 5;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
