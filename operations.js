1d9fc3cd15a9915641e0cbe178d59047e7e96aeb
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:55:21 +0530

    Delete operations.js

--- operations.js
+++
@@ -1,9 +0,0 @@
-// basic operations
-
-let a = 10;
-let b = 5;
-
-console.log(a + b); // Addition
-console.log(a - b); // Subtraction
-console.log(a * b); // Multiplication
-console.log(a / b); // Division
