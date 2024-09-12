ea6787bb34c836466654b60c05e81eb092f49831
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:20:04 +0530

    Add files via upload
    basic object structure in java script

---
+++ object.js
@@ -0,0 +1,7 @@
+let person = {          // <-- creating object
+    firstName: "Karan",
+    lastName: "Vani",
+    age: 21
+};
+
+console.log(person.firstName + " " + person.lastName); // Accessing object properties
