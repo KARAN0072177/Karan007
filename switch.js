5d4502ebe0871046747bae4a25059cf90ffe1635
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:33:12 +0530

    Switch case using in java script
    Switch case in js

---
+++ switch.js
@@ -0,0 +1,18 @@
+let day = 2;
+let dayName;
+
+switch(day) {
+    case 1:
+        dayName = "Sunday";
+        break;
+    case 2:
+        dayName = "Monday";
+        break;
+    case 3:
+        dayName = "Tuesday";
+        break;
+    default:
+        dayName = "Unknown day";
+}
+
+console.log(dayName);  // Output: Monday
