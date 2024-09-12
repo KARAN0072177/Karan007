05bc8accf6f6734bab5666f2620c7537519f6d83
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:07:03 +0530

    Create DataTypes.js
    Data Types in java script

---
+++ DataTypes.js
@@ -0,0 +1,9 @@
+// Data Types in Java-Script
+
+let age = 25;        // Number
+let isStudent = true; // Boolean
+let school = "ABC";   // String
+
+console.log(age);
+console.log(isStudent);
+console.log(school);
