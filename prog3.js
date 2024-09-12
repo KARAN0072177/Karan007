49594c399fb71f93c128a85e5b6d4ce132ee9ab2
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 23:16:53 +0530

    Update prog3.js

--- prog3.js
+++ prog3.js
@@ -1,11 +1,25 @@
-/* Program to calculate the sum of the digits of a number inputed by the user.*/
-let number=prompt("Input the number: ");
-let temp=number;
-let sum=0;
-while (temp>0) {
-    let n=temp%10;
-    sum=sum+n;
-    temp=temp/10;
-}
-console.log(" ");
-console.log("The sum of the digits of "+number+" is "+sum);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        /* Program to calculate the sum of the digits of a number input by the user. */
+        let number = prompt("Input the number: ");
+        let temp = number;
+        let sum = 0;
+        
+        while (temp > 0) {
+            let n = temp % 10; 
+            sum += n;  
+            temp = Math.floor(temp / 10);  // Remove the last digit using math.floor function
+        }
+        
+        console.log(" ");
+        console.log("The sum of the digits of " + number + " is " + sum);
+    </script>
+</body>
+</html>
