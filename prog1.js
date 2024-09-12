aff7ff326dc3bdfe0c7c41909a697892e8c332bb
Shivam Gupta <kapilmohangupta20@gmail.com>
2024-09-12 21:37:48 +0530

    12/09/2024

--- prog1.js
+++ prog1.js
@@ -1,3 +1,4 @@
+/* Program to print the table of a specified number.*/
 let number=54;
 for (let index = 1; index <=10; index++) {
     console.log(number + " * "+index+" =  "+number*index);
---
+++ prog2.js
@@ -0,0 +1,27 @@
+/* Program to give the grade to the student according the marks earned*/
+let marks = prompt("Enter marks: ");
+let grade;
+if (marks>=90 && marks<=100) {
+    grade='A+';
+}
+else if (marks>=80 && marks<90) {
+    grade='A';
+}
+else if (marks>=60 && marks<80) {
+    grade='B';
+}
+else if (marks>=40 && marks<60) {
+    grade='C';
+}
+else if (marks>=30 && marks<40) {
+    grade='D';
+}
+else if (marks>=0 && marks<30) {
+    grade='F';
+}
+else
+{
+    grade='0';
+}
+console.log("")
+console.log("The Grade obtained by student at "+marks+" is "+grade+".")
