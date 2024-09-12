6e5c19268b5374cc99298bdf868b4c14c931db0b
KARAN0072177 <karanvani2003@gmail.com>
2024-09-12 18:11:36 +0530

    Create ifelse.js
    basic if else statements in  java script

---
+++ ifelse.js
@@ -0,0 +1,16 @@
+// basic if-else statements
+
+let num = 10;
+
+if (num > 0) 
+{
+    console.log("Positive number");    
+} 
+else if (num < 0) 
+{
+    console.log("Negative number");
+} 
+else 
+{
+    console.log("It's zero");
+}
