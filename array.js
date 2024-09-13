eb69d97978508d4d5c7cdf23e492637d8a315880
KARAN0072177 <karanvani2003@gmail.com>
2024-09-13 18:46:17 +0530

    Delete array.js

--- array.js
+++
@@ -1,6 +0,0 @@
-let languages = ["C++", "React", "Express.js"];
-console.log(languages[0]); // Access first element  (index starts with 0)
-
-// Adding a new element
-languages.push("Node.js");
-console.log(languages);
