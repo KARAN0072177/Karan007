72142e796d5daa06405ab69f7507df42edb2217c
KARAN0072177 <karanvani2003@gmail.com>
2024-09-13 18:47:20 +0530

    Delete object.js

--- object.js
+++
@@ -1,7 +0,0 @@
-let person = {          // <-- creating object
-    firstName: "Karan",
-    lastName: "Vani",
-    age: 21
-};
-
-console.log(person.firstName + " " + person.lastName); // Accessing object properties
