216aa87848d476a090b7796a0683174026d9c91c
KARAN0072177 <karanvani2003@gmail.com>
2024-09-13 18:36:57 +0530

    Add files via upload

---
+++ math_func.js
@@ -0,0 +1,36 @@
+// 1. Math.abs()   Returns the absolute value of a number
+console.log(Math.abs(-5));  // 5
+
+// 2. Math.ceil() Rounds a number up to the next largest integer.
+console.log(Math.ceil(7.3));  // 8
+
+// 3. Math.floor() Rounds a number down to the previous integer.
+console.log(Math.floor(4.9));  // 4
+
+// 4. Math.round() Rounds a number to the nearest integer.
+console.log(Math.round(4.7));  // 5
+console.log(Math.round(4.3));  // 4
+
+// 5. Math.max() Returns the maximum number from a set of numbers.
+console.log(Math.max(1, 3, 2));  // 3
+
+// 6. Math.min() Returns the minimum number from a set of numbers. 
+console.log(Math.min(1, 3, 2));  // 1
+
+// 7. Math.pow() Returns the base raised to the power of the exponent.
+console.log(Math.pow(2, 3));  // 8 (2 raised to the power of 3)
+
+// 8. Math.sqrt() retruns a square root of gives number
+console.log(Math.sqrt(16));  // 4
+
+// 9. Math.random() Returns a random floating-point number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random());  // Random number between 0 and 1
+
+// 10. Math.trunc() runcates the decimal part of a number, leaving only the integer part.
+console.log(Math.trunc(4.9));  // 4
+console.log(Math.trunc(-4.9));  // -4
+
+// 11. Math.sign() Returns the sign of a number. It returns 1 for positive numbers, -1 for negative numbers, and 0 for zero.
+console.log(Math.sign(5));  // 1
+console.log(Math.sign(-5));  // -1
+console.log(Math.sign(0));  // 0
