c3d0aa276bfce80ff7f2564e2c8cf0d658e429cd
KARAN0072177 <karanvani2003@gmail.com>
2024-09-14 22:10:40 +0530

    Delete DataTypes.js

--- DataTypes.js
+++
@@ -1,9 +0,0 @@
-// Data Types in Java-Script
-
-let age = 25;        // Number
-let isStudent = true; // Boolean
-let school = "ABC";   // String
-
-console.log(age);
-console.log(isStudent);
-console.log(school);
